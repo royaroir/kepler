db69002326ba10dba4d94bbaafce4de7eccd0cf9
royaroir <roya.alagheband@outlook.com>
2024-12-18 16:00:43 +0800

    Add files via upload

---
+++ assets/logo-kepler-motion/logo kepler motion_edge.js
@@ -0,0 +1,9 @@
+
+(function(compId){"use strict";var _=null,y=true,n=false,x6='rgba(255,255,255,0.00)',x1='5.0.0',x3='rgba(0,0,0,0)',lf='left',tp='top',g='image',e8='${Pasted5}',x2='5.0.0.375',e7='${Pasted6}',i='none';var g4='Pasted5.svg',g5='Pasted6.svg';var im='images/',aud='media/',vid='media/',js='js/',fonts={},opts={'gAudioPreloadPreference':'auto','gVideoPreloadPreference':'auto'},resources=[],scripts=[],symbols={"stage":{v:x1,mv:x1,b:x2,stf:i,cg:i,rI:n,cn:{dom:[{id:'Pasted5',t:g,r:['179px','204px','138px','54px','auto','auto'],f:[x3,im+g4,'0px','0px']},{id:'Pasted6',t:g,r:['191px','204px','14px','19px','auto','auto'],f:[x3,im+g5,'0px','0px']}],style:{'${Stage}':{isStage:true,r:[undefined,undefined,'550px','751px'],overflow:'hidden',f:[x6]}}},tt:{d:1108.4004855639,a:y,data:[["eid70",tp,0,1108,"easeInOutQuad",e7,'-257px','204px'],["eid80",lf,1000,0,"easeOutExpo",e8,'179px','179px'],["eid84",lf,1108,0,"easeOutExpo",e7,'191px','191px'],["eid87",tp,0,1000,"easeInOutCubic",e8,'776px','204px'],["eid88",tp,1022,0,"easeInOutCubic",e8,'204px','204px']]}}};AdobeEdge.registerCompositionDefn(compId,symbols,fonts,scripts,resources,opts);})("EDGE-528833107");
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;Edge.registerEventBinding(compId,function($){
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${Stage}","mouseover",function(sym,e){});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+})})(AdobeEdge.$,AdobeEdge,"EDGE-528833107");
